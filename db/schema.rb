f08611ac41d6953fa149be1bbc85a85ec718d798
nemanjamandic <nemanjans84@gmail.com>
2015-10-25 01:51:12 +0000

    added asociations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024225505) do
+ActiveRecord::Schema.define(version: 20151025005004) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151024225505) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
